2f6b2854e0871506ac3493e687e7afdd768fb3ed
junhyeok <toughhyeok96@gmail.com>
2022-03-11 16:08:02 +0900

    git reset

---
+++ src/main/java/com/junhyeok/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.junhyeok.book.springboot.web.dto;
+
+import com.junhyeok.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
